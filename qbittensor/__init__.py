5f227dc7c323975af3ed7546953f9f6a3b3c862f
sirentangled <sirentangled@gmail.com>
2025-10-09 11:45:26 -0600

    Update __init__.py

--- qbittensor/__init__.py
+++ qbittensor/__init__.py
@@ -17,6 +17,6 @@
 # DEALINGS IN THE SOFTWARE.
 
 
-__version__ = "1.2.4"
+__version__ = "1.2.5"
 version_split = __version__.split(".")
 __spec_version__ = (1000 * int(version_split[0])) + (10 * int(version_split[1])) + (1 * int(version_split[2]))
